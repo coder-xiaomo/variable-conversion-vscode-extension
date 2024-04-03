695bcb830d6d30d226d63804cffcced0381fd38e
zhangbk1 <v-zhangbk@chinatelecom.cn>
2024-04-03 10:27:50 +0800

    SupportCase 枚举前添加支持新命名方式需要做的修改；package.json 添加 description

--- src/type-definition/support-case-type.ts
+++ src/type-definition/support-case-type.ts
@@ -1,10 +1,26 @@
+/**
+ * When support a new case, there's something we need to do.
+ *
+ * Code:
+ * - Add `commands`, `menus` parts in package.json (and package-comment.jsonc)
+ * - Add main conversion logic in src/main-code/variable-conversion.ts
+ *
+ * Test:
+ * - Add test case type definition in src/type-definition/test-case-type.ts
+ * - Add test case in src/test/test-case.ts
+ *
+ * Docs:
+ * - Add type definition in below `SupportCase` enum
+ * - Modify `description` in package.json
+ * - Add changes in CHANGELOG.md
+ */
 export enum SupportCase {
     /**
      * 小驼峰 / 驼峰命名
      * Camel Case
      * e.g. fooBar
      * 
-     * @alias: camelCase / CamelCase / camel_case / CAMEL_CASE
+     * @alias: camelCase / CamelCase / camel case / camel_case / CAMEL_CASE
      * @since 2024-04-02
      */
     CAMEL_CASE,
@@ -14,7 +30,7 @@ export enum SupportCase {
      * Pascal Case
      * e.g. FooBar
      * 
-     * @alias: pascalCase / PascalCase / pascal_case / PASCAL_CASE
+     * @alias: pascalCase / PascalCase / pascal case / pascal_case / PASCAL_CASE
      * @since 2024-04-02
      */
     PASCAL_CASE,
@@ -24,8 +40,8 @@ export enum SupportCase {
      * Kebab Case / Spinal Case
      * e.g. foo-bar
      * 
-     * @alias: kebabCase / KebabCase / kebab_case / KEBAB_CASE
-     *         spinalCase / SpinalCase / spinal_case / SPINAL_CASE
+     * @alias: kebabCase / KebabCase / kebab case / kebab_case / KEBAB_CASE
+     *         spinalCase / SpinalCase / spinal case / spinal_case / SPINAL_CASE
      * @since 2024-04-02
      */
     KEBAB_CASE,
@@ -35,17 +51,27 @@ export enum SupportCase {
      * Snake Case
      * e.g. foo_bar
      * 
-     * @alias: snakeCase / SnakeCase / snake_case / SNAKE_CASE
+     * @alias: snakeCase / SnakeCase / snake case / snake_case / SNAKE_CASE
      * @since 2024-04-02
      */
     SNAKE_CASE,
+
+    /**
+     * 驼峰蛇形命名
+     * Camel Snake Case
+     * e.g. Foo_Bar
+     * 
+     * @alias: camelSnakeCase / CamelSnakeCase / camel snake case / camel_snake_case / CAMEL_SNAKE_CASE
+     * @since 2024-04-02
+     */
+    CAMEL_SNAKE_CASE,
 
     /**
      * 下划线大写
      * Snake Upper Case
      * e.g. FOO_BAR
      * 
-     * @alias: snakeUpperCase / SnakeUpperCase / snake_upper_case / SNAKE_UPPER_CASE
+     * @alias: snakeUpperCase / SnakeUpperCase / snake upper case / snake_upper_case / SNAKE_UPPER_CASE
      * @since 2024-04-02
      */
     SNAKE_UPPER_CASE,
@@ -55,7 +81,7 @@ export enum SupportCase {
      * Upper Case
      * e.g. FOO_BAR / FOOBAR
      * 
-     * @alias: upperCase / UpperCase / upper_case / UPPER_CASE
+     * @alias: upperCase / UpperCase / upper case / upper_case / UPPER_CASE
      * @since 2024-04-02
      */
     UPPER_CASE,
@@ -65,18 +91,8 @@ export enum SupportCase {
      * Lower Case
      * e.g. foo_bar / foobar
      * 
-     * @alias: lowerCase / LowerCase / lower_case / LOWER_CASE
+     * @alias: lowerCase / LowerCase / lower case / lower_case / LOWER_CASE
      * @since 2024-04-02
      */
     LOWER_CASE,
-
-    /**
-     * 驼峰蛇形命名
-     * Camel Snake Case
-     * e.g. Foo_Bar
-     * 
-     * @alias: camelSnakeCase / CamelSnakeCase / camel_snake_case / CAMEL_SNAKE_CASE
-     * @since 2024-04-02
-     */
-    CAMEL_SNAKE_CASE,
 }
