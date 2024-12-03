d3a4b0d79f43c22b53bf8e3259385bbb1a954d37
张博凯 <codingzhang@126.com>
2024-12-03 23:50:44 +0800

    补充几处函数头注释

--- src/main-code/user-configuration.ts
+++ src/main-code/user-configuration.ts
@@ -1,9 +1,15 @@
 const vscode = require('vscode');
 
+/**
+ * 获取用户配置项
+ *
+ * @param configKey 配置项的键
+ * @returns 配置项的值
+ * @since 2024-07-29
+ */
 function getUserConfigurations(configKey: string) {
     const config = vscode.workspace.getConfiguration('variable-conversion');
 
-    // 获取 disableFormat 配置项
     const configValue = config.get(configKey);
     console.log('configValue:', configValue);
     return configValue;
